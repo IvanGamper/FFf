57396824ca1a517e8cf9265a9e75adaf9107787e
IvanGamper <ivangamper1991@gmail.com>
2024-12-21 11:56:46 +0100

    Update Main.java

--- cd/src/cd/Main.java
+++ cd/src/cd/Main.java
@@ -2,6 +2,6 @@ package cd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo, Welt!");
+        System.out.println("Hallo, schöne Welt!");
     }
 }
